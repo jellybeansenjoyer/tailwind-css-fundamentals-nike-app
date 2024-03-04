356cfa8bd49685dd839a7f0e08ee65daf04feffc
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-03-04 23:53:16 +0530

    skeleton of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
-   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello World!
-      </h1>
-   )
-}
+const App = ()=>(
+  <main className = "relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+        Hero
+    </section>
+
+    <section className="padding">
+        PopularProducts
+    </section>
+
+    <section className="padding">
+        SuperQuality
+    </section>
+
+    <section className="padding">
+        Services
+    </section>
+
+    <section className="padding-x py-10">
+        SpecialOffers
+    </section>
+
+  </main>
+);
+
+export default App;
