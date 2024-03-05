e793885031686d253dd51ca51711c490b92e41b7
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-03-05 12:46:35 +0530

    all sections screated

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,15 @@ const App = ()=>(
     <section className="padding-x py-10">
         SpecialOffers
     </section>
-
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x padding-t bg-black pb-8 text-white">
+      Footer
+    </section>
   </main>
 );
 
