5c2eab8530b11f96612df5414ab4f8d3a2e4eebb
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-03-06 16:32:35 +0530

    designing the hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
 import { statistics } from "../constants"
 const Hero = () => {
@@ -6,25 +7,30 @@ const Hero = () => {
     <section
         id="home"
         className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
-    <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 gap-10">
+    <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
       <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
       <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
         <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
         <br/>
         <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
       </h1>
-      <p>Discover stylish Nike arrivals, qualirt comfort and innovation for your active life.</p>
+      <p className="text-slate-gray font-montserrat mt-6 mb-14 text-lg leading-8 sm:max-w-sm">Discover stylish Nike arrivals, qualirt comfort and innovation for your active life.</p>
       <Button label = "Shop now" iconUrl={arrowRight}/>
       <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat)=>(
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
             </div>
           ))}
       </div>
     </div>
+    <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-center">
+      <img src = {bigShoe1} alt = "shoe collection" width={610} height={500} className="object-contain relative z-10">
+      </img>
+    </div>
     </section>
+    
   )
 }
 
